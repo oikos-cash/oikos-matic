05e017fdd9ba786e0500fc5446483ef3bfa45fc1
oikos1 <albert@oikos.cash>
2022-04-17 17:07:02 -0400

    add contracts

---
+++ contracts/bridge/BridgeToPolygon.sol
@@ -0,0 +1,34 @@
+pragma solidity 0.5.16;
+
+interface ChainPortMainBridge {
+    function depositTokens(address token, uint256 value, uint256 toChainId) external;
+    function burnTokens(address token, uint256 amount) external;
+}
+
+contract BridgeToPolygon {
+
+    address public chainPortMainBridge = 0x2CD90158baAE285010A5Ed7C549C2E5B4C0715F4;
+    address public oks = 0x18aCf236eB40c0d4824Fb8f2582EBbEcD325Ef6a;
+
+    /**
+     * @dev Requires prior approval
+     * @notice Bridges tokens to Polygon.
+     * @param amount The amount to bridge.
+     */
+    function bridge(uint amount) external {
+        (bool success, ) = address(chainPortMainBridge).delegatecall(abi.encodeWithSignature("depositTokens(address, uint256, uint256)", oks, amount, 3));
+    }
+
+}
+
+contract UnBridgeToBSC is BridgeToPolygon {
+
+    /**
+     * @dev Requires prior approval
+     * @notice Unbridge tokens back to BSC.
+     * @param amount The amount to unbridge.
+     */
+    function unbridge(uint amount) external {
+        (bool success, ) = address(chainPortMainBridge).delegatecall(abi.encodeWithSignature("burnTokens(address, uint256)", oks, amount));
+    }
+}
